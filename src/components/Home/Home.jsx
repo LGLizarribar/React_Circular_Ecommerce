d828f7608bfc5f45b01b978669392205d7ff9e6e
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-05-14 20:39:00 +0200

    FEAT: add product images to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllProductsAsync } from '../../redux/slices/product.slice';
 import './Home.scss';
 
 const Home = () => {
+    const products = useSelector(state => state.products.all);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!products.length) dispatch(getAllProductsAsync());;
+    }, []);
 
     return (
-        <main class="main">
-            <h1 class="main__title">Welcome to Circular</h1>
-            <p class="main__content">Get what you need. Sell what you don't.</p>
+        <main className="main">
+            <h1 className="main__title">Welcome to Circular</h1>
+            <p className="main__content">Get what you need. Sell what you don't.</p>
+            <div className="main__images">
+                {products.map(p => <div  key={p._id}>
+                    {p.productImage && <img src={p.productImage} className='products__image' alt="{p.name}"/>}
+                </div>)}
+            </div>
         </main>
     )
 }
